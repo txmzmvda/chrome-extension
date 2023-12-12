a38865ef2a36c8acec13ac6465630bc236f9fb44
txmzmvda <tz.zmuda@gmail.com>
2023-12-12 22:31:39 +0100

    Added files with the first chrome app

---
+++ index.js
@@ -0,0 +1,53 @@
+let myLeads = []
+let oldLeads = []
+const inputEl = document.getElementById("input-el")
+const inputBtn = document.getElementById("input-btn")
+const ulEl = document.getElementById("ul-el")
+const saveBtn = document.getElementById("save-btn")
+const deleteBtn = document.getElementById("delete-btn")
+
+const leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"))
+
+
+if (leadsFromLocalStorage) {
+    myLeads = leadsFromLocalStorage
+    render(myLeads)
+}
+
+function render(leads) {
+    let listItems = ""
+    for (let i = 0; i < leads.length; i++) {
+        listItems += `
+            <li>
+                <a target='_blank' href ='${leads[i]}'>${leads[i]}</a>
+            </li>
+            `
+    }
+
+    ulEl.innerHTML = listItems
+}
+
+deleteBtn.addEventListener("dblclick", function () {
+    localStorage.clear()
+    myLeads = []
+    render(myLeads)
+})
+
+inputBtn.addEventListener("click", function () {
+    myLeads.push(inputEl.value)
+    console.log(myLeads)
+    localStorage.setItem("myLeads", JSON.stringify(myLeads))
+    render(myLeads)
+    inputEl.value = ""
+})
+
+saveBtn.addEventListener("click", function () {
+    chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+        myLeads.push(tabs[0].url)
+        localStorage.setItem("myLeads", JSON.stringify(myLeads))
+        render(myLeads) 
+    })
+})
+
+
+
